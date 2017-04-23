a1fa64eb866bc79fc08759aa883b07cae14ff4db
TobiahRex <tobiah.rex@gmail.com>
2017-04-23 16:57:02 +0900

    created a list type.

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 import { graphql, buildSchema } from 'graphql';
 
 const schema = buildSchema(`
-type Video {
-  id: ID,
-  title: String,
-  duration: Int,
-  watched: Boolean,
-}
-type Query {
-  video: Video
-  videos: [Video]
-}
-
-type Schema {
-  query: Query
-}
-`);
-
-  const resolvers = {
-    video: () => ({
-      id: '1',
-      title: 'Foo',
-      duration: 180,
-      watched: true,
-    }),
-    videos: () => videos,
-  };
-
-  const query = `
-  query myFirstQuery {
-    video {
-      id,
-    }
+  type Video {
+    id: ID,
+    title: String,
+    duration: Int,
+    watched: Boolean,
   }
-  `;
+  type Query {
+    video: Video
+    videos: [Video]
+  }
 
-  graphql(schema, query, resolvers)
-  .then(results => console.log(JSON.stringify(results, null, 2)))
-  .catch(console.error)
+  type Schema {
+    query: Query
+  }
+`);
+const videoA = {
+  id: 'a',
+  title: 'babel-node',
+  duration: 240,
+  watched: true,
+}
+const videoB = {
+  id: 'b',
+  title: 'node.js',
+  duration: 120,
+  watched: false,
+}
+const videos = [videoA, videoB];
+const resolvers = {
+  video: () => ({
+    id: '1',
+    title: 'Foo',
+    duration: 180,
+    watched: true,
+  }),
+  videos: () => videos,
+};
+
+const query = `
+query myFirstQuery {
+  videos {
+    id,
+  }
+}
+`;
+
+graphql(schema, query, resolvers)
+.then(results => console.log(JSON.stringify(results, null, 2)))
+.catch(console.error)
